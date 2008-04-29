86791bb7ff83695e322bb57b8a86dd4bb2ef50ee
Jimmy Conner <cigamit@cacti.net>
2008-04-29 05:26:24 +0000

    Flowview is now only PA 2.0 compatible
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/flowview/trunk@230 67214365-cc29-0410-bfbe-843132f0f3d7

--- setup.php
+++ setup.php
@@ -22,17 +22,49 @@
  +-------------------------------------------------------------------------+
 */
 
-function plugin_init_flowview() {
-	global $plugin_hooks;
-	$plugin_hooks['config_arrays']['flowview'] = 'flowview_config_arrays';
-	$plugin_hooks['draw_navigation_text']['flowview'] = 'flowview_draw_navigation_text';
-	$plugin_hooks['config_settings']['flowview'] = 'flowview_config_settings';
-	$plugin_hooks['poller_bottom']['flowview'] = 'flowview_poller_bottom';
+function plugin_flowview_install () {
+	api_plugin_register_hook('flowview', 'config_arrays', 'flowview_config_arrays', 'setup.php');
+	api_plugin_register_hook('flowview', 'draw_navigation_text', 'flowview_draw_navigation_text', 'setup.php');
+	api_plugin_register_hook('flowview', 'config_settings', 'flowview_config_settings', 'setup.php');
+	api_plugin_register_hook('flowview', 'poller_bottom', 'flowview_poller_bottom', 'setup.php');
+
+	api_plugin_register_realm('flowview', 'flowview.php,flowview_devices.php,flowview_schedules.php', 'Flow Viewer', 1);
+}
+
+function plugin_flowview_uninstall () {
+	// Do any extra Uninstall stuff here
+}
+
+function plugin_flowview_check_config () {
+	// Here we will check to ensure everything is configured
+	flowview_check_upgrade ();
+	return true;
+}
+
+function plugin_flowview_upgrade () {
+	// Here we will upgrade to the newest version
+	flowview_check_upgrade ();
+	return false;
 }
 
 function flowview_version () {
+	return plugin_flowview_version();
+}
+
+function flowview_check_upgrade () {
+	$current = plugin_flowview_version ();
+	$current = $current['version'];
+	$old = read_config_option('plugin_flowview_version');
+	if ($current != $old)
+		flowview_setup_table ();
+	// Set the new version
+	db_execute("REPLACE INTO settings (name, value) VALUES ('plugin_flowview_version', '$current')");
+}
+
+
+function plugin_flowview_version () {
 	return array( 'name' 	=> 'flowview',
-			'version' 	=> '0.5',
+			'version' 	=> '0.6',
 			'longname'	=> 'FlowView',
 			'author'	=> 'Jimmy Conner',
 			'homepage'	=> 'http://cactiusers.org',
@@ -42,12 +74,7 @@ function flowview_version () {
 }
 
 function flowview_config_arrays () {
-	global $user_auth_realms, $user_auth_realm_filenames, $menu;
-
-	$user_auth_realms[68]='Flow Viewer';
-	$user_auth_realm_filenames['flowview.php'] = 68;
-	$user_auth_realm_filenames['flowview_devices.php'] = 68;
-	$user_auth_realm_filenames['flowview_schedules.php'] = 68;
+	global $menu;
 
 	$temp = $menu["Utilities"]['logout.php'];
 	unset($menu["Utilities"]['logout.php']);
@@ -145,100 +172,77 @@ function flowview_poller_bottom () {
 }
 
 function flowview_setup_table () {
-	global $config, $database_default;
-	include_once($config["library_path"] . "/database.php");
-	$sql = "show tables from `" . $database_default . "`";
+	$data = array();
+	$data['columns'][] = array('name' => 'ip', 'type' => 'varchar(32)', 'NULL' => false, 'default' => '');
+	$data['columns'][] = array('name' => 'host', 'type' => 'varchar(255)', 'NULL' => false, 'default' => '');
+	$data['columns'][] = array('name' => 'time', 'type' => 'int(20)', 'NULL' => false, 'default' => '0');
+	$data['keys'][] = array('name' => 'ip', 'columns' => 'ip');
+	$data['type'] = 'HEAP';
+	$data['comment'] = 'Plugin Flowview - DNS Cache to help speed things up';
+	api_plugin_db_table_create ('flowview', 'plugin_flowview_dnscache', $data);
 
-	$result = db_fetch_assoc($sql) or die (mysql_error());
+	$data = array();
+	$data['columns'][] = array('name' => 'id', 'type' => 'int(12)', 'NULL' => false, 'auto_increment' => true);
+	$data['columns'][] = array('name' => 'name', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'folder', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'allowfrom', 'type' => 'varchar(32)', 'NULL' => false, 'default' => '0');
+	$data['columns'][] = array('name' => 'port', 'type' => 'int(12)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'nesting', 'type' => 'varchar(4)', 'NULL' => false, 'default' => '-1');
+	$data['columns'][] = array('name' => 'version', 'type' => 'varchar(12)', 'NULL' => false, 'default' => '5');
+	$data['columns'][] = array('name' => 'rotation', 'type' => 'int(12)', 'NULL' => false, 'default' => '1439');
+	$data['columns'][] = array('name' => 'expire', 'type' => 'int(3)', 'NULL' => false, 'default' => '7');
+	$data['columns'][] = array('name' => 'compression', 'type' => 'int(1)', 'NULL' => false, 'default' => '0');
+	$data['primary'] = 'id';
+	$data['keys'][] = array('name' => 'folder', 'columns' => 'folder');
+	$data['type'] = 'HEAP';
+	$data['comment'] = 'Plugin Flowview - List of Devices to collect flows from';
+	api_plugin_db_table_create ('flowview', 'plugin_flowview_devices', $data);
 
-	$tables = array();
-	$sql = array();
+	$data = array();
+	$data['columns'][] = array('name' => 'id', 'type' => 'int(12)', 'NULL' => false, 'auto_increment' => true);
+	$data['columns'][] = array('name' => 'name', 'type' => 'varchar(255)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'device', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'startdate', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'starttime', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'enddate', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'endtime', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'tosfields', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'tcpflags', 'type' => 'varchar(32)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'protocols', 'type' => 'varchar(8)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'sourceip', 'type' => 'varchar(255)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'sourceport', 'type' => 'varchar(255)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'sourceinterface', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'sourceas', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'destip', 'type' => 'varchar(255)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'destport', 'type' => 'varchar(255)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'destinterface', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'destas', 'type' => 'varchar(64)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'statistics', 'type' => 'int(3)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'printed', 'type' => 'int(3)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'includeif', 'type' => 'int(2)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'sortfield', 'type' => 'int(2)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'cutofflines', 'type' => 'int(4)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'curoffoctets', 'type' => 'varchar(8)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'resolve', 'type' => 'varchar(2)', 'NULL' => false);
+	$data['primary'] = 'id';
+	$data['keys'][] = array('name' => 'name', 'columns' => 'name');
+	$data['type'] = 'MyISAM';
+	$data['comment'] = 'Plugin Flowview - List of Saved Flow Queries';
+	api_plugin_db_table_create ('flowview', 'plugin_flowview_queries', $data);
 
-	foreach($result as $index => $arr) {
-		foreach ($arr as $t) {
-			$tables[] = $t;
-		}
-	}
-
-	if (!in_array('plugin_flowview_dnscache', $tables)) {
-		$sql[] = "CREATE TABLE plugin_flowview_dnscache (
-				ip varchar(32) NOT NULL default '',
-				host varchar(255) NOT NULL default '',
-				`time` int(20) NOT NULL default '0',
-				KEY ip (ip)
-				) TYPE=HEAP;";
-		$sql[] = "REPLACE INTO user_auth_realm VALUES (68, 1);";
-	}
-
-	if (!in_array('plugin_flowview_devices', $tables)) {
-		$sql[] = "CREATE TABLE plugin_flowview_devices (
-				  id int(12) NOT NULL auto_increment,
-				  name varchar(64) NOT NULL,
-				  folder varchar(64) NOT NULL,
-				  allowfrom varchar(32) NOT NULL default '0',
-				  port int(12) NOT NULL,
-				  nesting varchar(4) NOT NULL default '-1',
-				  version varchar(12) NOT NULL default '5',
-				  rotation int(12) NOT NULL default '1439',
-				  expire int(3) NOT NULL default '7',
-				  compression int(1) NOT NULL default '0',
-				  PRIMARY KEY  (id),
-				  KEY folder (folder),
-				) TYPE=MyISAM;";
-		$sql[] = "INSERT INTO plugin_flowview_devices (name, folder, port) VALUES ('Default', 'Router', 2055)";
-	}
-
-	if (!in_array('plugin_flowview_queries', $tables)) {
-		$sql[] = "CREATE TABLE `plugin_flowview_queries` (
-				  `id` int(12) NOT NULL auto_increment,
-				  `name` varchar(255) NOT NULL,
-				  `device` varchar(32) NOT NULL,
-				  `startdate` varchar(32) NOT NULL,
-				  `starttime` varchar(32) NOT NULL,
-				  `enddate` varchar(32) NOT NULL,
-				  `endtime` varchar(32) NOT NULL,
-				  `tosfields` varchar(32) NOT NULL,
-				  `tcpflags` varchar(32) NOT NULL,
-				  `protocols` varchar(8) NOT NULL,
-				  `sourceip` varchar(255) NOT NULL,
-				  `sourceport` varchar(255) NOT NULL,
-				  `sourceinterface` varchar(64) NOT NULL,
-				  `sourceas` varchar(64) NOT NULL,
-				  `destip` varchar(255) NOT NULL,
-				  `destport` varchar(255) NOT NULL,
-				  `destinterface` varchar(64) NOT NULL,
-				  `destas` varchar(64) NOT NULL,
-				  `statistics` int(3) NOT NULL,
-				  `printed` int(3) NOT NULL,
-				  `includeif` int(2) NOT NULL,
-				  `sortfield` int(2) NOT NULL,
-				  `cutofflines` int(4) NOT NULL,
-				  `cutoffoctets` varchar(8) NOT NULL,
-				  `resolve` varchar(2) NOT NULL,
-				  PRIMARY KEY  (`id`),
-				  KEY `name` (`name`)
-				) TYPE=MyISAM;";
-	}
-	if (!in_array('plugin_flowview_schedules', $tables)) {
-		$sql[] = "CREATE TABLE `plugin_flowview_schedules` (
-				  `id` int(12) NOT NULL auto_increment,
-				  `enabled` varchar(3) NOT NULL default 'on',
-				  `sendinterval` int(20) NOT NULL,
-				  `lastsent` int(20) NOT NULL,
-				  `start` datetime NOT NULL,
-				  `email` text NOT NULL,
-				  `savedquery` int(12) NOT NULL,
-				  PRIMARY KEY  (`id`),
-				  KEY `savedquery` (`savedquery`)
-				) TYPE=MyISAM;";
-	}
-
-
-	if (!empty($sql)) {
-		for ($a = 0; $a < count($sql); $a++) {
-			$result = mysql_query($sql[$a]);
-		}
-	}
+	$data = array();
+	$data['columns'][] = array('name' => 'id', 'type' => 'int(12)', 'NULL' => false, 'auto_increment' => true);
+	$data['columns'][] = array('name' => 'enabled', 'type' => 'varchar(3)', 'NULL' => false, 'default' => 'on');
+	$data['columns'][] = array('name' => 'sendinterval', 'type' => 'int(20)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'lastsent', 'type' => 'int(20)', 'NULL' => false);
+	$data['columns'][] = array('name' => 'start', 'type' => 'datetime', 'NULL' => false);
+	$data['columns'][] = array('name' => 'email', 'type' => 'text', 'NULL' => false);
+	$data['columns'][] = array('name' => 'savedquery', 'type' => 'int(12)', 'NULL' => false);
+	$data['primary'] = 'id';
+	$data['keys'][] = array('name' => 'savedquery', 'columns' => 'savedquery');
+	$data['type'] = 'HEAP';
+	$data['comment'] = 'Plugin Flowview - Scheduling for running and emails of saved queries';
+	api_plugin_db_table_create ('flowview', 'plugin_flowview_schedules', $data);
 }
 
 
