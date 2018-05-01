26b916256ca5ccae55e0f8dd082a16cbdc16c411
netniV <netniv@hotmail.com>
2018-05-01 07:10:37 +0000

    Wait for document ready before applying function handlers #22

--- functions.php
+++ functions.php
@@ -178,98 +178,100 @@ function flowview_display_report() {
 	swfobject.embedSWF('open-flash-chart.swf', 'chartpackets', '98%', height, '9.0.0', 'expressInstall.swf', {'data-file':'<?php print urlencode($config["url_path"] . "plugins/flowview/flowview.php?session=" . $sessionid . "&action=chartdata&exclude=" . get_request_var('exclude') . "&type=packets&title=$rname");?>', 'id':'chartpackets'});
 	swfobject.embedSWF('open-flash-chart.swf', 'chartflows', '98%', height, '9.0.0', 'expressInstall.swf', {'data-file':'<?php print urlencode($config["url_path"] . "plugins/flowview/flowview.php?session=" . $sessionid . "&action=chartdata&exclude=" . get_request_var('exclude') . "&type=flows&title=$rname");?>', 'id':'chartflows'});
 
-	$('#bytes').click(function() {
-		if (!$('#bytes').is(':checked')) {
-			$('#wrapperbytes').hide();
-			$.get('flowview.php?action=updatesess&type=bytes&value=');
+	$(function() {
+		$('#bytes').click(function() {
+			if (!$('#bytes').is(':checked')) {
+				$('#wrapperbytes').hide();
+				$.get('flowview.php?action=updatesess&type=bytes&value=');
+			}else{
+				$('#wrapperbytes').show();
+			}
+		});
+
+		$('#packets').click(function() {
+			if (!$('#packets').is(':checked')) {
+				$('#wrapperpackets').hide();
+				$.get('flowview.php?action=updatesess&type=packets&value=');
+			}else{
+				$('#wrapperpackets').show();
+			}
+		});
+
+		$('#flows').click(function() {
+			if (!$('#flows').is(':checked')) {
+				$('#wrapperflows').hide();
+				$.get('flowview.php?action=updatesess&type=flows&value=');
+			}else{
+				$('#wrapperflows').show();
+			}
+		});
+
+		$('#table').click(function() {
+			if (!$('#table').is(':checked')) {
+				$('#flowcontent').hide();
+				$.get('flowview.php?action=updatesess&type=table&value=');
+			}else{
+				$.get('flowview.php?action=updatesess&type=table&value=on');
+				$('#flowcontent').show();
+			}
+		});
+
+		$('#clear').click(function() {
+			loadPageNoHeader('flowview.php?header=false&action=view&clear=true&tab='+$('#tab').val());
+		});
+
+		$('#exclude').change(function() {
+			loadPageNoHeader('flowview.php?header=false&action=view&exclude='+$('#exclude').val()+'&tab='+$('#tab').val());
+		});
+
+		if ($('#table').is(':checked') || <?php print (isset_request_var('stat_report') ? (get_nfilter_request_var('stat_report') == 99 ? 'true':'false'):'true');?>) {
+			$('#flowcontent').show();
 		}else{
+			$('#flowcontent').hide();
+		}
+
+		if ($('#bytes').is(':checked')) {
 			$('#wrapperbytes').show();
 		}
-	});
 
-	$('#packets').click(function() {
-		if (!$('#packets').is(':checked')) {
-			$('#wrapperpackets').hide();
-			$.get('flowview.php?action=updatesess&type=packets&value=');
-		}else{
+		if ($('#packets').is(':checked')) {
 			$('#wrapperpackets').show();
 		}
-	});
 
-	$('#flows').click(function() {
-		if (!$('#flows').is(':checked')) {
-			$('#wrapperflows').hide();
-			$.get('flowview.php?action=updatesess&type=flows&value=');
-		}else{
+		if ($('#flows').is(':checked')) {
 			$('#wrapperflows').show();
 		}
-	});
 
-	$('#table').click(function() {
-		if (!$('#table').is(':checked')) {
-			$('#flowcontent').hide();
-			$.get('flowview.php?action=updatesess&type=table&value=');
-		}else{
-			$.get('flowview.php?action=updatesess&type=table&value=on');
-			$('#flowcontent').show();
-		}
-	});
+		$.tablesorter.addParser({
+			id: 'bytes',
+			is: function(s) {
+				return false;
+			},
+        
+			format: function(s) {
+				if (s.indexOf('MB') > 0) {
+					loc=s.indexOf('MB');
+					return s.substring(0,loc) * 1024 * 1024;
+				}else if (s.indexOf('KB') > 0) {
+					loc=s.indexOf('KB');
+					return s.substring(0,loc) * 1024;
+				}else if (s.indexOf('Bytes') > 0) {
+					loc=s.indexOf('Bytes');
+					return s.substring(0,loc);
+				}else if (s.indexOf('GB') > 0) {
+					loc=s.indexOf('GB');
+					return s.substring(0,loc) * 1024 * 1024 * 1024;
+				}else if (s.indexOf('TB') > 0) {
+					loc=s.indexOf('TB');
+					return s.substring(0,loc) * 1024 * 1024 * 1024 * 1024;
+				}else{
+					return s;
+				}
+			},
 
-	$('#clear').click(function() {
-		loadPageNoHeader('flowview.php?header=false&action=view&clear=true&tab='+$('#tab').val());
-	});
+			type: 'numeric'
+		});
 
-	$('#exclude').change(function() {
-		loadPageNoHeader('flowview.php?header=false&action=view&exclude='+$('#exclude').val()+'&tab='+$('#tab').val());
-	});
-
-	if ($('#table').is(':checked') || <?php print (isset_request_var('stat_report') ? (get_nfilter_request_var('stat_report') == 99 ? 'true':'false'):'true');?>) {
-		$('#flowcontent').show();
-	}else{
-		$('#flowcontent').hide();
-	}
-
-	if ($('#bytes').is(':checked')) {
-		$('#wrapperbytes').show();
-	}
-
-	if ($('#packets').is(':checked')) {
-		$('#wrapperpackets').show();
-	}
-
-	if ($('#flows').is(':checked')) {
-		$('#wrapperflows').show();
-	}
-
-    $.tablesorter.addParser({
-        id: 'bytes',
-        is: function(s) {
-            return false;
-        },
-        format: function(s) {
-			if (s.indexOf('MB') > 0) {
-				loc=s.indexOf('MB');
-				return s.substring(0,loc) * 1024 * 1024;
-			}else if (s.indexOf('KB') > 0) {
-				loc=s.indexOf('KB');
-				return s.substring(0,loc) * 1024;
-			}else if (s.indexOf('Bytes') > 0) {
-				loc=s.indexOf('Bytes');
-				return s.substring(0,loc);
-			}else if (s.indexOf('GB') > 0) {
-				loc=s.indexOf('GB');
-				return s.substring(0,loc) * 1024 * 1024 * 1024;
-			}else if (s.indexOf('TB') > 0) {
-				loc=s.indexOf('TB');
-				return s.substring(0,loc) * 1024 * 1024 * 1024 * 1024;
-			}else{
-				return s;
-			}
-        },
-        type: 'numeric'
-    });
-
-	$(function() {
 		$('#sorttable').tablesorter({
 			widgets: ['zebra'],
 			widgetZebra: { css: ['even', 'odd'] },
